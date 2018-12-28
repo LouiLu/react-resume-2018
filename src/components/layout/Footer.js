515e1a6204fcd29e44ed121766b1e72b344e617d
Smallulu <junhaol1206@gmail.com>
2018-12-27 16:12:11 -0800

    deploy error with bebel settings

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,18 +5,18 @@ export default function Footer() {
     <div>
       <footer id="footer" className="bg-dark text-white p-2 text-center">
         <div className="social-icons">
-          <a href="">
+          <a href="https://linkedin.com/junhaol">
             <i className="fab fa-linkedin-in fa-sm" />
           </a>
-          <a href="">
+          <a href="https://github.com/junhaol">
             <i className="fab fa-github" />
           </a>
-          <a href="">
+          {/* <a href="">
             <i className="fab fa-twitter" />
           </a>
           <a href="">
             <i className="fab fa-facebook-f" />
-          </a>
+          </a> */}
         </div>
         <small>
           Coypright &copy; {new Date().getFullYear()} Smallulu Resume
